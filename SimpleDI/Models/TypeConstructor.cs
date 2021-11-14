7a3e368898a20a47eeb280c4700e6daab1c3a1e9
kostyamospan <37243242+kostyamospan@users.noreply.github.com>
2021-11-14 19:03:22 +0200

    changes: arguments parser getConstructor new return type

---
+++ SimpleDI/Models/TypeConstructor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SimpleDI.Models
+{
+    public class TypeConstructor
+    {
+        public IEnumerable<FunctionArgument> Arguments { get; set; }
+    }
+}
--- SimpleDI/Reflection/ArgumentsParser.cs
+++ SimpleDI/Reflection/ArgumentsParser.cs
@@ -13,18 +13,21 @@ namespace SimpleDI.Reflection
         /// Gets all arguments of a constructor of a given type
         /// Requires type to have single constructor
         /// </summary>
-        public static IEnumerable<FunctionArgument> GetConstructorArguments(Type type)
+        public static TypeConstructor GetConstructor(Type type)
         {
             var constructors = type.GetConstructors();
             if (constructors.Length > 1) throw new TooManyConstructorsException(type);
 
-            return !constructors.Any()
-                ? Enumerable.Empty<FunctionArgument>()
-                : constructors
-                    .First()
-                    .GetParameters()
-                    .Select(v =>
-                        new FunctionArgument() {Name = v.Name, Type = v.ParameterType});
+            return new TypeConstructor()
+            {
+                Arguments = !constructors.Any()
+                    ? Enumerable.Empty<FunctionArgument>()
+                    : constructors
+                        .First()
+                        .GetParameters()
+                        .Select(v =>
+                            new FunctionArgument() {Name = v.Name, Type = v.ParameterType})
+            };
         }
     }
 }
