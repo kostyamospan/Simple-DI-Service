32975876c343bf57b0f8db17f8349d149cc3e0d4
kostyamospan <37243242+kostyamospan@users.noreply.github.com>
2021-11-14 19:02:53 +0200

    added: CircularDependencyException.cs

---
+++ SimpleDI/Exceptions/CircularDependencyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleDI.Exceptions
+{
+    public class CircularDependencyException : DiException
+    {
+        public CircularDependencyException(Type type) 
+            : base($"Circular dependencies are unacceptable. Type: {nameof(type)}")
+        {
+        }
+    }
+}
